d6a52b72e120b840c6214729b1c0fb6bce6b7b9f
GabrielGaudi <gabriel.rosalvo@etec.sp.gov.br>
2024-08-14 10:47:15 -0300

    atualização 14/8 animação

--- js/classes.js
+++ js/classes.js
@@ -1,13 +1,54 @@
 class obj{
-    constructor(x,y,width,height,img){
+    quadro = 0
+    tempo = 0 
+
+    constructor(x,y,width,height,imagem){
         this.x = x
         this.y = y
         this.width = width
         this.height = height 
-        this.img = img
+        this.imagem = imagem
     }
     draw(){
-        let img = new img()
+        let img = new Image()
+        img.src = this.imagem
+        canvas.drawImage(img, this.x, this.y, this.width, this.height)
+    }
+    animacao(vel,limite,nomeImg){
+        this.tempo ++
+        if(this.tempo >= vel){
+            this.tempo = 0
+            this.quadro ++
+        }
+
+        if(this.quadro >= limite){
+            this.quadro = 0
+        }
+
+        this.imagem = 'img/'+nomeImg+this.quadro+".png"
     }
 }
 
+class Bg extends obj{
+    move(velocidade, limite, posicao){
+        this.x -= velocidade
+
+        if(this.x < limite){
+            this.x = posicao
+        }
+    }
+}
+
+class Ground extends obj{
+    move(velocidade, limite, posicao){
+        this.x -= velocidade
+
+        if(this.x < limite){
+            this.x = posicao
+        }
+    }
+}
+
+class Bird extends obj{
+    
+}
--- js/script.js
+++ js/script.js
@@ -1,10 +1,53 @@
-const canvas = document.getElementById("canvas").getContext("2d")
+const canvas = document.getElementById('canvas').getContext('2d')
 
-let bg = 
+let fundo = new Bg(0,0,500,900,'img/sky.png')
+let fundo2 = new Bg(500,0,500,900,'img/sky.png')
+let ground = new Ground(0,700,500,200,'img/ground.png')
+let ground2 = new Ground(500,700,500,200,'img/ground.png')
+
+let bird = new Bird(50,400,63,51, 'img/bird0.png')
+
+let canoRand = (Math.random(1) * 10)
 
 
+document.addEventListener("keydown", function(tecla){
+    if(tecla.keyCode == 32){
+        bird.y -= 120
+    }
+})
 
+function movimento(){
+    bird.y += 3
+}
+
+function draw(){
+    fundo.draw()
+    fundo2.draw()
+
+    ground.draw()
+    ground2.draw()
+
+    bird.draw()
+}
+
+function update(){
+    fundo.move(1,-500,0)
+    fundo2.move(1,0,500)
+
+    ground.move(2,-500,0)
+    ground2.move(2,0,500)
+
+    bird.animacao(7,4,'bird')
+
+    movimento()
+}
 
 function main(){
+    canvas.clearRect(0,0,500,900)
+    update()
     draw()
+    requestAnimationFrame(main)
+    console.log(canoRand)
 }
+
+main()
